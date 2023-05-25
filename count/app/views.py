2d29fbfde2f2f34bee709283f47873ce020314b2
Hyeyoung-Eum <djagpdud125@korea.ac.kr>
2023-05-25 16:43:04 +0900

    230525 글자수 세기 시작, 기본 틀 완성

---
+++ count/app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def count(request):
+    #logics here
+    return render(request, 'count.html')
+
+def result(request):
+    text=request.POST['text']
+    total_len=len(text)
+    no_blank_len=len(text.replace(' ',''))
+    word=len(text.split())
+    onegozi=round(len(text)/200,1)
+    return render(request, 'result.html', {'text':text,'total_len':total_len,'no_blank_len':no_blank_len, 'word':word, 'onegozi':onegozi})
